76b38181212d0e85992b9bd65e1896e93742c3ac
VanPhamNe <119727678+VanPhamNe@users.noreply.github.com>
2025-11-07 21:10:24 +0700

    map 1,map 2,map 3

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,9 +6,14 @@ public class Tower : MonoBehaviour
     [Header("Tower Setup")]
     [SerializeField] private Transform towerHead;
     [SerializeField] private float rotationSpeed;
+    [SerializeField] private float attackRange;
     private void Update()
     {
         RotateTowardsEnemy();
+    }
+    private void FindRandomEnemyInRange()
+    {
+
     }
     private void RotateTowardsEnemy()
     {
@@ -16,5 +21,10 @@ public class Tower : MonoBehaviour
         Quaternion lookRotation = Quaternion.LookRotation(direction); // Xoay de huong ve enemy
         Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles; // Noi suy giua huong hien tai va huong can xoay
         towerHead.rotation = Quaternion.Euler(rotation);// Ap dung goc xoay moi cho tower head
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(towerHead.position, attackRange);
     }
 }
